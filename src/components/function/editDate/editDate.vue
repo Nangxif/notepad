73d3eccc979872be07aea6584d67eebc96519e9d
Nangxi <575981390@qq.com>
2019-03-16 17:30:53 +0800

    添加记日子功能，修改所有功能界面

--- src/components/function/editDate/editDate.vue
+++ src/components/function/editDate/editDate.vue
@@ -2,11 +2,40 @@
   <div class="editDate">
     <head-top :is-back="isBack" page-title="记日子"></head-top>
     <div class="editDate_wrapper">
+    	<input type="text" name="day_name" class="day_name" placeholder="事件名称" />
+    	<a @click="selectDate" class="date_select">{{indelibility}}</a>
+    	<textarea class="remark">备注</textarea>
+    	<a @click="selectLevel" class="level_select"><span :style="{'background-color': selectColor}">{{level}}</span></a>
+    	<a @click="create_Date" class="editDate_create">创建</a>
         <calendar
 		    v-model="calendarShow"
 		    :defaultDate="defaultDate"
 		    @change="handelChange">
 		</calendar>
+	
+
+		<!-- 事件等级选择弹框 -->
+		<div class="level_select_modal"  @click="selectLevel">
+			<div class="level_select_wrapper">
+				<a @click.stop="selectLevelColor(index)" v-for="(btn,index) in levelBtn">{{btn.text}}<span :style="{'background-color':btn.color}"><i class="icon iconfont icon-gou" v-if="index==0"></i></span></a>
+				<!-- <a @click.stop="selectLevelColor(0)">重要<span style="background-color: #ef7676;"><i class="icon iconfont icon-gou"></i></span></a>
+				<a @click.stop="selectLevelColor(1)">比较重要<span style="background-color: #35b8f3;"></span></a>
+				<a @click.stop="selectLevelColor(2)">普普通通<span style="background-color: #9cff29;"></span></a>
+				<a @click.stop="selectLevelColor(3)">比较不重要<span style="background-color: #fed640;"></span></a>
+				<a @click.stop="selectLevelColor(4)">不重要<span style="background-color: #ccb7af;"></span></a> -->
+			</div>
+		</div>
+		<!-- 创建成功弹框 -->
+		<div class="create_modal">
+			<div class="create_wrapper">
+				<div class="create_title">
+					{{indelibility}}
+				</div>
+				<p class="create_desc">{{levelBtn[selectIndex].desc}}</p>
+				<a class="create_modal_close" @click="closeCreate">取消</a>
+			</div>
+		</div>
+
     </div>
   </div>
 </template>
@@ -17,16 +46,70 @@ export default {
     data(){
         return{
             "isBack": true,
-            calendarShow: true,
-			defaultDate: new Date()
+            indelibility: new Date().getFullYear()+"年"+(new Date().getMonth()+1)+"月"+new Date().getDate()+"日",
+            level: "重要",//初始事件等级
+            calendarShow: false,
+			defaultDate: new Date(),//初始默认时间
+			selectColor: "#ef7676",//事件等级初始颜色
+			levelBtn:[{
+				text:"重要",
+				color:"#ef7676",
+				desc:"这一天发生了一件重要的事情，我很开心！！！"
+			},{
+				text:"比较重要",
+				color:"#35b8f3",
+				desc:"这一天发生了一件比较重要的事情，记一下吧！"
+			},{
+				text:"普普通通",
+				color:"#9cff29",
+				desc:"这一天发生了一件普普通通的事情，记个流水账~"
+			},{
+				text:"比较不重要",
+				color:"#fed640",
+				desc:"这一天发生了一件比较不重要的事情，那我记它干嘛？"
+			},{
+				text:"不重要",
+				color:"#ccb7af",
+				desc:"这一天发生了一件不重要的事情，算了不想吐槽！"
+			}],
+			selectIndex:0
         }
     },
     components:{
         headTop
     },
     methods:{
-    	handelChange(){
-
+    	selectDate(){
+    		this.calendarShow = true;
+    	},
+    	selectLevel(e){
+    		e.stopPropagation();
+    		if($(".level_select_modal").css("display")=="block"){
+    			$(".level_select_modal").fadeOut();
+    			$(".level_select_wrapper").css("transform","translateY(100%)");
+    		}else{
+    			$(".level_select_modal").fadeIn();
+    			$(".level_select_wrapper").css("transform","translateY(0%)");
+    		}
+    		
+    	},
+    	selectLevelColor(ele){
+    		$(".level_select_wrapper a").find("span").children().remove();
+    		$(".level_select_wrapper a").eq(ele).find("span").append(`<i class="icon iconfont icon-gou"></i>`);
+    		this.level = $(".level_select_wrapper a").eq(ele).text();
+    		this.selectColor = $(".level_select_wrapper a").eq(ele).find("span").css("background-color");
+    		this.selectIndex = ele;
+    	},
+    	handelChange(data){
+    		this.indelibility = new Date().getFullYear(data)+"年"+(new Date(data).getMonth()+1)+"月"+new Date(data).getDate()+"日";
+    		console.log(data);
+    	},
+    	create_Date(){
+    		$(".create_modal").fadeIn();
+    		$(".create_wrapper").addClass("create_wrapper_bg_"+this.selectIndex);
+    	},
+    	closeCreate(){
+    		$(".create_modal").fadeOut();
     	}
     }
 }
@@ -44,7 +127,204 @@ export default {
         overflow-y: auto;
         z-index: 501;
     }
-    
+    .day_name{
+    	display: block;
+        margin: 0 auto;
+        margin-top: 1rem;
+        padding: 0px 0.5rem;
+        width: 90%;
+        height: 2.5rem;
+        line-height: 2.5rem;
+        border: 0px;
+        border-bottom: 1px solid #fed640;
+        font-size: 1rem;
+        color: #979797;
+        outline: none;
+    }
+
+    .date_select,.level_select{
+    	display: block;
+        margin: 0 auto;
+        margin-top: 1rem;
+        padding: 0.2rem;
+        width: 90%;
+        height: 2.5rem;
+        border: 0px;
+        border: 1px solid #fed640;
+        font-size: 1rem;
+        color: #979797;
+        outline: none;
+        border-radius: .2rem;
+    }
+    .date_select{
+    	padding: 0px 0.4rem;
+        line-height: 2.5rem;   	
+    }
+    .level_select span{
+    	display: inline-block;
+        padding: 0px 0.4rem;
+        line-height: 2rem;
+    	width: 100%;
+    	text-align: center;
+    	height: 100%;
+    	color: white;
+    }
+    .remark{
+    	display: block;
+        margin: 0 auto;
+        margin-top: 1rem;
+        padding: 0px 0.5rem;
+        width: 90%;
+        line-height: 2.5rem;
+        min-height: 10rem;
+        border: 0px;
+        border: 1px solid #fed640;
+        font-size: 1rem;
+        color: #979797;
+        outline: none;
+        border-radius: .2rem;
+    }
+    .editDate_create{
+    	position: relative;
+        display: block;
+        margin: 0 auto;
+        margin-top: 1rem;
+        width: 90%;
+        text-align: center;
+        height: 2.5rem;
+        line-height: 2.5rem;
+        font-size: 1rem;
+        background-color: #fed640;
+        border-radius: 1.25rem;
+        text-decoration: none;
+        color: white;
+    }
+
+	/*事件等级弹框*/
+    .level_select_modal{
+    	display: none;
+    	position: absolute;
+    	left: 0px;
+    	top: 0px;
+    	width: 100%;
+    	height: 100%;
+    	background: rgba(0,0,0,.4);
+	    z-index: 9999;
+    }
+    .level_select_wrapper{
+    	position: absolute;
+    	left: 0px;
+    	bottom: 0px;
+    	width: 100%;
+    	background-color: white;
+		transform: translateY(100%);
+		transition: all .3s ease-out;
+    }
+    .level_select_wrapper a{
+    	display: block;
+		padding-left: 0.5rem;
+		padding-right: 0.5rem;
+		width: 100%;
+		height: 3rem;
+		line-height: 3rem;
+		font-size: 1rem;
+        color: #979797;
+        border-top: 1px solid #eeeeee;
+    }
+    .level_select_wrapper a span{
+    	float: right;
+    	position: relative;
+    	display: inline-block;
+    	margin-top: 1rem;
+    	width: 1rem;
+    	text-align: center;
+    	height: 1rem;
+    	line-height: 1rem;
+    }
+	.level_select_wrapper a span i{
+		font-size: 1rem;
+		color: white;
+	}
+
+	/*创建成功弹框*/
+	.create_modal{
+    	display: none;
+    	position: absolute;
+    	left: 0px;
+    	top: 0px;
+    	width: 100%;
+    	height: 100%;
+    	background: rgba(0,0,0,.4);
+	    z-index: 9999;
+    }
+    .create_wrapper{
+    	margin: auto;
+    	position: absolute;
+		left: 0px;
+		right: 0px;
+		top: 0px;
+		bottom: 0px;
+		width: 20rem;
+		height: 17.6rem;
+		background-repeat: no-repeat;
+		background-size: 100% auto;
+    }
+    .create_wrapper_bg_0{
+		background-image: url('./date2.jpg');
+    }
+    .create_wrapper_bg_1{
+		background-image: url('./date2.jpg');
+    }
+    .create_wrapper_bg_2{
+		background-image: url('./date3.jpg');
+    }
+    .create_wrapper_bg_3{
+		background-image: url('./date4.jpg');
+    }
+    .create_wrapper_bg_4{
+		background-image: url('./date5.jpg');
+    }
+    .create_wrapper_bg_5{
+		background-image: url('./date6.jpg');
+    }
+
+    .create_title{
+    	position: absolute;
+        left: 1.5rem;
+        top: 1rem;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        padding-bottom: .2rem;
+        font-size: 1.6rem;
+        color: #c7adad;
+        border-bottom: 2px solid #c7adad;
+    }
+    .create_desc{
+    	position: absolute;
+        left: 1.5rem;
+        top: 4.8rem;
+        padding-right: 1.5rem;
+        font-size: 1.2rem;
+        color: #c7adad;
+    }
+	.create_modal_close{
+		margin: auto;
+		position: absolute;
+		left: 0px;
+		right: 0px;
+		bottom: 1rem;
+        display: block;
+        width: 50%;
+        text-align: center;
+        height: 2.5rem;
+        line-height: 2.5rem;
+        font-size: 1rem;
+        background-color: #fed640;
+        border-radius: 1.25rem;
+        text-decoration: none;
+        color: white;
+	}
+
    
 
 
--- src/components/function/function.vue
+++ src/components/function/function.vue
@@ -2,15 +2,20 @@
   <div class="function">
     <head-top :is-back="isBack" page-title="功能"></head-top>
     <div class="function_wrapper">
-        <div class="function_item">
-            <i class="icon iconfont icon-xie"></i>写手账
+        <div class="function_item function_item_1">
+            <p class="function_item_title">
+                <i class="icon iconfont icon-xie"></i>写手账
+            </p>
+            <p class="function_item_desc">记录人生最美的瞬间</p>
+            <div class="function_item_tip_1">Diary</div>
         </div>
-        <router-link class="function_item" to="/function/editDate">
-            <i class="icon iconfont icon-shijian"></i>记日子
+        <router-link class="function_item function_item_2" to="/function/editDate">
+            <p class="function_item_title">
+                <i class="icon iconfont icon-shijian"></i>记日子
+            </p>
+            <p class="function_item_desc">生活需要纪念日</p>
+            <div class="function_item_tip_2">Memorial<br/>Day</div>
         </router-link>
-        <div class="function_item">
-            待开发……
-        </div>
     </div>
     <router-view></router-view>
     <foot-top></foot-top>
@@ -45,15 +50,14 @@ export default {
         overflow-y: auto;
     }
     .function_item{
+        position: relative;
         display: block;
         margin: 0 auto;
-        margin-top: 0.5rem;
+        margin-top: 2.5rem;
+        margin-bottom: 2.5rem;
         width: 90%;
-        text-align: center;
-        height: 6rem;
-        line-height: 6rem;
-        font-size: 1.9rem;
-        border: 1px solid #fed640;
+        height: 18rem;
+        /*line-height: 6rem;*/
         border-radius: .5rem;
         background-color: #fed640;
         color: white;
@@ -62,5 +66,62 @@ export default {
         margin-right: .2rem;
         font-size: 1.9rem;
         color: white;
+    }
+    .function_item_1{
+        background-image: url('./banner1.jpg');
+        background-repeat: no-repeat;
+        background-size: 100% auto;
+    }
+    .function_item_2{
+        background-image: url('./banner2.jpg');
+        background-repeat: no-repeat;
+        background-size: 100% auto;
+    }
+
+    .function_item_title{
+        position: absolute;
+        left: 1.5rem;
+        top: 1rem;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        padding-bottom: .2rem;
+        font-size: 1.9rem;
+        border-bottom: 2px solid white;
+    }
+    .function_item_desc{
+        position: absolute;
+        left: 1.5rem;
+        top: 4.8rem;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        font-size: 1.2rem;
+    }
+    .function_item_tip_1{
+        position: absolute;
+        right: 1.5rem;
+        top: -2rem;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        text-align: center;
+        height: 4rem;
+        line-height: 4rem;
+        font-size: 1.6rem;
+        color: white;
+        font-weight: 600;
+        background-color: #553520;
+    }
+    .function_item_tip_2{
+        position: absolute;
+        right: 1.5rem;
+        top: -2rem;
+        padding-top: .45rem;
+        padding-left: .5rem;
+        padding-right: .5rem;
+        text-align: center;
+        height: 4rem;
+        font-size: 1.2rem;
+        color: white;
+        font-weight: 600;
+        background-color: #ddb68d;
     }
 </style>
